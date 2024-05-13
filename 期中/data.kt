2c56f456e88a6fb7321dabb60f37a5000b9f8da8
FJ-FTH <161728347+FJ-FTH@users.noreply.github.com>
2024-05-13 15:06:43 +0800

    data.kt

--- 期中/data.kt
+++ 期中/data.kt
@@ -7,7 +7,7 @@ import androidx.compose.runtime.Immutable
 data class Teacher(
     var tid: Int,
     var tname:String,
-    var tage:Int,
+    var tage:String,
     var temail: String,
     var tclass: String
 )
@@ -15,20 +15,21 @@ data class Teacher(
 data class Student (
     var sid: Int,
     var sname:String,
-    var sage:Int,
+    var sage:String,
     var semail: String,
     var sclass: String
 
 )
-val teachers = listOf(
-    Teacher(1,"张三",27,"1234567@qq.com","移动互联221班"),
-    Teacher(1,"张三",27,"1234567@qq.com","移动互联221班"),
-    Teacher(1,"张三",27,"1234567@qq.com","移动互联221班")
+val teachers = mutableListOf(
+    Teacher(1,"张三","27","1234567@qq.com","移动互联221班"),
+    Teacher(1,"张三","27","1234567@qq.com","移动互联221班"),
+    Teacher(1,"张三","27","1234567@qq.com","移动互联221班")
 )
-val students = listOf(
-    Student(7,"王二",21,"09876576@qq.com","移动互联222班"),
-    Student(7,"王二",21,"09876576@qq.com","移动互联222班"),
-    Student(7,"王二",21,"09876576@qq.com","移动互联222班"),
-    Student(7,"王二",21,"09876576@qq.com","移动互联222班"),
-    Student(7,"王二",21,"09876576@qq.com","移动互联222班")
+val students = mutableListOf(
+    Student(7,"王二","21","09876576@qq.com","移动互联222班"),
+    Student(7,"王二","21","09876576@qq.com","移动互联222班"),
+    Student(7,"王二","21","09876576@qq.com","移动互联222班"),
+    Student(7,"王二","21","09876576@qq.com","移动互联222班"),
+    Student(7,"王二","21","09876576@qq.com","移动互联222班")
 )
+
